84c3db3ff04ed3cc03be807560ddb8869a4c21d0
attraverso <annachiara.traverso@gmail.com>
2020-06-16 10:02:32 +0200

    improved visualization

--- index.php
+++ index.php
@@ -4,15 +4,30 @@
 
 $sentence = 'Lorem ipsum dolor sit amet consectetur adipisicing elit Est obcaecati labore voluptates blanditiis sunt beatae voluptatibus ad quod quasi nam vitae debitis laborum, expedita voluptate voluptatem libero unde Animi accusamus';
 
-var_dump($sentence);
+$sentence_length = strlen($sentence);
 
-$CHN_censorship_levels_of_nonsense = $_GET['badword'];
+$chinese_censorship_levels_of_nonsense = $_GET['badword'];
 
-$happy_censor = str_replace($CHN_censorship_levels_of_nonsense, '***', $sentence);
+$happy_censor = str_replace($chinese_censorship_levels_of_nonsense, '***', $sentence);
 
-echo $happy_censor;
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Automated censor</h1>
+  <p><b>Testo originale:</b></p>
+  <p><?php echo $sentence ?></p>
+  <p><b>Lunghezza del testo:</b> <?php echo $sentence_length ?></p>
+  <p><b>Parola da censurare:</b> <?php echo $chinese_censorship_levels_of_nonsense ?></p>
+  <p><b>Testo censurato:</b></p>
+  <p><?php echo $happy_censor?></p>
+</body>
+</html>
 
 <!-- Creare una variabile con un paragrafo di testo.
 Visualizzare a schermo il paragrafo con la relativa lunghezza e sostituire la badword passata in get con tre * -->
